3d81c407cbe68433b722b6bafbb8d6951596fb1a
Amanda <amanda141828@gmail.com>
2024-08-29 12:01:50 -0300

    Add files via upload

---
+++ # aula3.py
@@ -0,0 +1,15 @@
+# Variáveis em Python
+# Não há necessidade de declará-las no início do programa
+# Basta criar no meio do algoritmo com a aribuição
+
+teste = "Amanda"
+print(teste) # Imprime a string "Eduardo"
+print(type(teste)) # Imprime o tipo do variável, string
+teste = 10
+print(teste) # Imprime o número 10
+print(type(teste)) # Imprime o tipo da variável, que é int
+teste = 1.74 
+print(teste) # Imprime o tipo da variável, que é float
+teste = True
+print(teste) # Imprime o valor True
+print(type(teste)) # Imprime o tipo variável, que é boll
---
+++ 01-aula01.py
@@ -0,0 +1,19 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Amanda") # Comentário a linha de código
+print('Manda')
+print('Amanda "Manda" Espirito')
+print("Amanda 'Manda' Espirto") 
+print("Amanda\"Manda\"Espirito")
+
+teste = "oi"
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
