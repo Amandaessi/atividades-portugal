5a26a7eae7bcfc21261b62984870dcc7d7993123
Amanda <amanda141828@gmail.com>
2024-11-06 11:13:23 -0300

    Add files via upload

---
+++ 27-nome-nota.py
@@ -0,0 +1,13 @@
+# solicita os 5 números aos usuários
+alunos = []
+alunos = []
+
+
+for i in range(5)
+    aluno = input("Digite o nome aluno:")
+    nota = int(input("Digite a nota do aluno:")
+    alunos.append(aluno)
+    notas.append(nota)
+
+for i in rage (5)
+    print(f"O aluno {alunos[i]} tirou nota {notas[i]}")
